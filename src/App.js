573f7c8e0955f32eb2070d22f27d3b52595f3517
Ostap <55147391+BrunoUA96@users.noreply.github.com>
2022-08-18 23:30:07 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import './scss/app.scss';
+import PizzaCard from "./components/PizzaCard";
+import Header from "./components/Header";
+import Categories from "./components/Categories";
+import Sort from "./components/Sort";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="wrapper">
+            <Header/>
+            <div className="content">
+                <div className="container">
+                    <div className="content__top">
+                        <Categories/>
+                        <Sort/>
+                    </div>
+                    <h2 className="content__title">Все пиццы</h2>
+                    <div className="content__items">
+                        <PizzaCard title={'hello'} price={0}/>
+                        <PizzaCard title={'Pizza'} price={300}/>
+                        <PizzaCard title={'Test'}/>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/PizzaCard.jsx
@@ -0,0 +1,61 @@
+import React, {useState} from "react";
+
+function PizzaCard({title, price}) {
+
+    // Пример дискруктизации
+    // const obj = {a: 1, b: 2, c: 3};
+    // Обычный способ дисктруктиризации
+    // const a = obj.a;
+    // const b = obj.b;
+    // const c = obj.c;
+
+    // Simplify
+    // const {a, b, c} = {a: 1, b: 2, c: 3};
+    // console.log(a, b, c);
+
+    // useState Hook Example
+    const [countPizza, setPizzaCount] = useState(0);
+
+    return (
+        <div className="pizza-block">
+            <img
+                className="pizza-block__image"
+                src="https://dodopizza-a.akamaihd.net/static/Img/Products/Pizza/ru-RU/b750f576-4a83-48e6-a283-5a8efb68c35d.jpg"
+                alt="Pizza"
+            />
+            <h4 className="pizza-block__title">{title}</h4>
+            <div className="pizza-block__selector">
+                <ul>
+                    <li className="active">тонкое</li>
+                    <li>традиционное</li>
+                </ul>
+                <ul>
+                    <li className="active">26 см.</li>
+                    <li>30 см.</li>
+                    <li>40 см.</li>
+                </ul>
+            </div>
+            <div className="pizza-block__bottom">
+                <div className="pizza-block__price">от {price || price === 0 ? price : 'teste'}</div>
+                <button className="button button--outline button--add" onClick={() => setPizzaCount(countPizza + 1)}>
+                    <svg
+                        width="12"
+                        height="12"
+                        viewBox="0 0 12 12"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                    >
+                        <path
+                            d="M10.8 4.8H7.2V1.2C7.2 0.5373 6.6627 0 6 0C5.3373 0 4.8 0.5373 4.8 1.2V4.8H1.2C0.5373 4.8 0 5.3373 0 6C0 6.6627 0.5373 7.2 1.2 7.2H4.8V10.8C4.8 11.4627 5.3373 12 6 12C6.6627 12 7.2 11.4627 7.2 10.8V7.2H10.8C11.4627 7.2 12 6.6627 12 6C12 5.3373 11.4627 4.8 10.8 4.8Z"
+                            fill="white"
+                        />
+                    </svg>
+                    <span>Добавить</span>
+                    <i>{countPizza}</i>
+                </button>
+            </div>
+        </div>
+    );
+}
+
+export default PizzaCard;
